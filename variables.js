9e4a14e3a63851c4520d658a9dc41de0457d84e1
mohit <mohitbhusal11@gmail.com>
2024-07-20 15:45:55 +0530

    initial commit and also added variable file

---
+++ variables.js
@@ -0,0 +1,9 @@
+const userID = 11222334455
+let userName = "Mohit"
+var userAddress = "Main road no. 1" /////Dont use Var use only const and let
+userCity = "Bhopal"
+
+
+/////////////////How to print javascript code in console
+console.log(userID) //this is going to print userId
+console.table([userID, userName, userAddress, userCity])
