869065b41ed9e740581fd62015b9d4289343ba76
mohit <mohitbhusal11@gmail.com>
2024-07-21 22:36:52 +0530

    Revised strings

---
+++ strings.js
@@ -0,0 +1,9 @@
+const name = "Mohit"
+const number = 5
+
+console.log("my name is "+name+" I have total "+number+ " repository") //this is the old way to concetinate string
+
+console.log(`my name is ${name} I have total ${number} repository`) //this is the mordern way to concetinate string
+
+
+//Checkout string functions on Docs or Articles
